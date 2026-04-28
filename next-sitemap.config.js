7193bd31dd5493f20cdf22fee0aac821af756de6
mrpitch <mrpitch@outlook.com>
2026-04-28 20:16:19 +0200

    fix: update domain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-	siteUrl: process.env.SITE_URL || 'https://mskanzlei.mrpitch.rocks',
+	siteUrl: process.env.SITE_URL || 'https://ms-kanzlei.de',
 	generateRobotsTxt: true, // (optional)
 	// ...other options
 }
